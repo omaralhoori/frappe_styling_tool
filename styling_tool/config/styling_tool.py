dcd7c60288559e0d78ac2dbd4f88c9e516f5388a
Omar Alhori <omaralhoori@gmail.com>
2021-03-18 17:21:34 +0700

    Editing config file

--- styling_tool/config/styling_tool.py
+++ styling_tool/config/styling_tool.py
@@ -4,13 +4,24 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"label": _("Icons"),
+			"label": _("Theme Customization"),
 			"icon": "fa fa-star",
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Desk Icons",
 					"description": _("Desk Modules Icons"),
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Desk Module",
+					"description": _("Desk Modules"),
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Theme Settings",
 					"onboard": 1,
 				}
 			]
