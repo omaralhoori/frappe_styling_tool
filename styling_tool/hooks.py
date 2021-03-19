fb5f0e89226d135ec4b3361be5b170224f7b1611
Omar Alhori <omaralhoori@gmail.com>
2021-03-19 22:21:04 +0700

    Adding custom style

--- styling_tool/hooks.py
+++ styling_tool/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/styling_tool/css/styling_tool.css"
+app_include_css = "/assets/styling_tool/css/custom.css"
 # app_include_js = "/assets/styling_tool/js/styling_tool.js"
 
 # include js, css files in header of web template
